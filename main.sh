4dda59115b1bc788d5c644d3a332ce97d323e1ae
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-06-21 07:24:06 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,4 @@ if [ ! -f "alist" ];then
   curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
-  mkdir /workspace/data
-  rm 
-  cp config.json /workspace/data/config.json
 fi
